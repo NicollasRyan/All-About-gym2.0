2dca93b434eda6bf925fc880c799ace92e7818a7
Nicollas Ryan <nbarretoduarte@gmail.com>
2024-07-26 17:04:49 -0300

    feat: removing comment

--- src/components/CardWeek/index.tsx
+++ src/components/CardWeek/index.tsx
@@ -87,37 +87,6 @@ export function CardWeek({ day, link }: CardType) {
     }
   }, [day, trainingId]);
 
-  // useEffect(() => {
-  //   const fetchAllDocuments = async (collectionName: string) => {
-  //     try {
-  //       const querySnapshot = await getDocs(collection(db, collectionName));
-  //       const trainingData = querySnapshot.docs.map((doc) => ({
-  //         ...doc.data(),
-  //       }));
-  //       console.log("<=", trainingData);
-  //       setWorkout(trainingData);
-  //     } catch (error) {
-  //       console.error("Erro ao obter documento:", error);
-  //     }
-  //   };
-
-  //   if (day === "Domingo") {
-  //     fetchAllDocuments("sunday_training");
-  //   } else if (day === "Segunda-feira") {
-  //     fetchAllDocuments("monday_training");
-  //   } else if (day === "Terça-feira") {
-  //     fetchAllDocuments("tuesday_training");
-  //   } else if (day === "Quarta-feira") {
-  //     fetchAllDocuments("wednesday_training");
-  //   } else if (day === "Quinta-feira") {
-  //     fetchAllDocuments("thursday_training");
-  //   } else if (day === "Sexta-feira") {
-  //     fetchAllDocuments("friday_training");
-  //   } else if (day === "Sábado") {
-  //     fetchAllDocuments("saturday_training");
-  //   }
-  // }, [day]);
-
   return (
     <CardContainer onClick={handleWorkout}>
       <CardContent>
